124449a75f6ec4f5e2059c88aeec6667a72ccb27
justkindathere <justkindathere13@gmail.com>
2024-04-20 15:02:12 -0400

    Test

--- kubejs/startup_scripts/CustomItems.js
+++ kubejs/startup_scripts/CustomItems.js
@@ -9,7 +9,7 @@ GTCEuStartupEvents.registry('gtceu:material', e => {
 
     e.create('andesite_alloy')
         .ingot()
-        .toolStats(ToolProperty.Builder.of(3.0F, 2.0F, 192, 2))
+        //.toolStats(ToolProperty.Builder.of(3.0F, 2.0F, 192, 2))
         .components('2x andesite', '2x iron')
         .color(0x747878).iconSet(GTMaterialIconSet.DULL)
         .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_GEAR, GTMaterialFlags.GENERATE_SMALL_GEAR, GTMaterialFlags.GENERATE_FRAME, GTMaterialFlags.GENERATE_ROD, GTMaterialFlags.GENERATE_LONG_ROD, GTMaterialFlags.GENERATE_DENSE)
