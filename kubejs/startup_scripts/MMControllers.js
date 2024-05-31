1048daf36c9af487316dc439499e6ad960a5e711
ACrazyD <merlinsky2000@gmail.com>
2024-05-31 10:36:09 -0400

    Structures go BRRR

---
+++ kubejs/startup_scripts/MMControllers.js
@@ -0,0 +1,9 @@
+console.info('MM: Registering Non-Greg Multiblocks Prt1...')
+MMEvents.registerControllers((e) => {
+
+	e.create('void_well').name('Void Well Controller').type('mm:machine')
+
+})//End Of File
+
+
+
