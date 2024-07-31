f10a2cc422f74b8eae09dd482cddd45dc3122201
ACrazyD <merlinsky2000@gmail.com>
2024-07-31 18:26:59 -0400

    Texture Patching

--- kubejs/server_scripts/Mods/Create/Mixing.js
+++ kubejs/server_scripts/Mods/Create/Mixing.js
@@ -2,8 +2,13 @@ console.info('Create Mixing: Registering recipes...')
 ServerEvents.recipes((e) => {
 
 	//Quartz Solution
-	//e.recipes.createMixing([Fluid.water(10), 'gtceu:nether_quartz_dust'], [Fluid.of('kubejs:quartz_solution')]).heated()
-	e.recipes.createMixing([Fluid.of('kubejs:quartz_solution')], [Fluid.water(10), 'gtceu:nether_quartz_dust']).heated()
+	e.recipes.createMixing([Fluid.of('kubejs:quartz_solution', 20)], [Fluid.water(10), 'gtceu:nether_quartz_dust']).heated()
+	e.recipes.createMixing('refinedstorage:quartz_enriched_iron',[
+		Fluid.of('kubejs:quartz_solution', 100),
+		'minecraft:iron_ingot'
+	]
+	).heated()
+
 	//BronzeRefactor
 	e.recipes
 		.createMixing('2x gtceu:bronze_ingot', [
--- kubejs/server_scripts/Mods/RefinedStorage/recipes.js
+++ kubejs/server_scripts/Mods/RefinedStorage/recipes.js
@@ -2,5 +2,6 @@ console.info('RefinedStorage: Registering recipes...')
 ServerEvents.recipes(e => {
 
     e.remove({ mod: 'refinedstorage' })
+    e.remove({ mod: 'rsrequestify' })
 
 })//End Of File
