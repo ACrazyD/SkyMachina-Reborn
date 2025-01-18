f838cab153cf23b4d2ed2c53cba369f5d3af7af0
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2025-01-18 08:54:17 -0500

    Delete kubejs directory

--- kubejs/startup_scripts/CustomItems/cb.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry('block', (e) => {
-
-e.create('dust')
-    .color(0x5f5f5f)
-    .displayName('Dust')
-    .material('sand')
-    .tagBlock('mineable/shovel')
-
-})//End Of File
