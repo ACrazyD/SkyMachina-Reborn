0c22a1c9c33b873988e9a774866524ba70459b5c
ACrazyD <merlinsky2000@gmail.com>
2025-01-18 09:57:15 -0500

    Remove the Stuffs

--- AAA-GitHub-UPLOAD/Changelog.md
+++ AAA-GitHub-UPLOAD/Changelog.md
@@ -1,3 +1,20 @@
+[ALPHA-0.0.2]
+## Changed
+    # Quests
+    # KubeJS Related
+        - Updated Removal Lists
+    # Other
+## Removed
+    # Quests
+    # KubeJS Related
+    # Other
+## Added
+    # Quests
+    # KubeJS Related
+    # Other
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 [ALPHA-0.0.1] - Primary Alpha Release
 ## Changed
     # Quests
---
+++ kubejs/server_scripts/TEMP-Disable.js
@@ -0,0 +1,66 @@
+ServerEvents.recipes(e => {
+
+    // This File Only Exists To Remove Mods Pending Rebuild of Recipes
+    // This File will One Day Be Removed When All Recipes Have Been Rebuilt
+    // From The SkyMachina Devs - Hello And Thanks For Testing, and Exploring this with us!
+
+    // Remove all Ranom Mods
+    e.remove({ mod: 'actuallyadditions'})
+    e.remove({ mod: 'hostilenetworks'})
+    e.remove({ mod: 'modularrouters'})
+    e.remove({ mod: 'modernindustrialrouters'})
+    e.remove({ mod: 'mekanisticrouters'})
+    e.remove({ mod: 'railcraft'})
+    e.remove({ mod: 'powah'})
+    e.remove({ mod: 'justdirethings'})
+    e.remove({ mod: 'productivebees'})
+    e.remove({ mod: 'industrialforegoing'})
+    e.remove({ mod: 'industrialforegoingsouls'})
+    e.remove({ mod: 'computercraft'})
+    e.remove({ mod: 'enderio'})
+    e.remove({ mod: 'exdeorum'})
+    e.remove({ mod: 'torchmaster'})
+    e.remove({ mod: 'pocketgps'})
+    e.remove({ mod: 'easy_villagers'})
+    e.remove({ mod: 'waystones'})
+    e.remove({ mod: 'buildinggadgets2'})
+    e.remove({ mod: 'enderchests'})
+    e.remove({ mod: 'endertanks'})
+
+
+    // Remove All AE2 and Related Mods
+    e.remove({ mod: 'advanced_ae'})
+    e.remove({ mod: 'ae2'})
+    e.remove({ mod: 'ae2_mega_things'})
+    e.remove({ mod: 'appflux'})
+    e.remove({ mod: 'appmek'})
+    e.remove({ mod: 'extendedae'})
+    e.remove({ mod: 'arseng'})
+    e.remove({ mod: 'soulplied_energistics'})
+    e.remove({ mod: 'fulleng'})
+
+    // Remove All Ars Nouveau and Related Mods
+    e.remove({ mod: 'ars_additions'})
+    e.remove({ mod: 'ars_elemental'})
+    e.remove({ mod: 'ars_nouveau'})
+
+    // Remove All Mekanism and Related Mods
+    e.remove({ mod: 'mekanism'})
+    e.remove({ mod: 'mekanism_tools'})
+    e.remove({ mod: 'mekanism_generators'})
+    e.remove({ mod: 'mekanismadditions'})
+
+    // Remove All MultiBlock and Related Mods
+    e.remove({ mod: 'modular_machinery_reborn'})
+    e.remove({ mod: 'modular_machinery_reborn_ars'})
+    e.remove({ mod: 'modular_machinery_reborn_mekanism'})
+
+    // Remove All Sophisticated Storage and Funcional Storage
+    e.remove({ mod: 'sophisticatedbackpacks'})
+    e.remove({ mod: 'sophisticatedstorage'})
+    e.remove({ mod: 'functionalstorage'})
+
+    // Remove All Mystical Agriculture and Related Mods
+    e.remove({ mod: 'mysticalagriculture'})
+})
+// End of File
